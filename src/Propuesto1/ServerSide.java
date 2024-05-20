05398531b8a3972a27d76950d386625c00d0c454
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-19 22:38:42 -0500

    Se agrega el registro en el servidor para que se pueda conectar el cliente

--- src/Propuesto1/ServerSide.java
+++ src/Propuesto1/ServerSide.java
@@ -1,10 +1,13 @@
 package Propuesto1;
 
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
 
 public class ServerSide {
-    
     public static void main(String[] args) throws Exception {
+        System.setProperty("java.rmi.server.hostname", "192.168.0.6");
+        
+        LocateRegistry.createRegistry(1099);
         Stock pharmacy = new Stock();
         pharmacy.addMedicine("Paracetamol", 3.2f, 10);
         pharmacy.addMedicine("Mejoral", 2.0f, 20);
